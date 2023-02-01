a1b2833cf35bd64d499ccc07745e5453b9e4cfb5
iAahmad <abbassyed012@gmail.com>
2023-02-01 14:39:35 +0000

    Initial commit

---
+++ pattern_1_54321.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=n)
+
+        {
+            /* code */
+            cout<<n-j+1;
+            j= j+1;
+
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+    
+ }
