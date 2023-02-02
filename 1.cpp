dd12b8864d1491dd0b0dd3dc44641cd2f09f3c11
Abbas Syed Ahmad <abbassyed012@gmail.com>
2023-02-02 21:19:00 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+	int t, R1, R2, R3, R4;
+	cin >> t;
+	while (t--) {
+		cin >> R1 >> R2 >> R3 >> R4;
+		if (R1 == 0 && R2 == 0 && R3 == 0 && R4 == 0)
+		{
+			/* code */
+			cout << "IN" << endl;
+
+		} else {
+			cout << "OUT" << endl;
+		}
+	}
+
+}
---
+++ Nearest_kit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+	int t, x;
+	cin >> t;
+	while (t--) {
+		cin >> x;
+		if (x <= 50)
+		{
+			/* code */
+			cout << "LEFT" << endl;
+		} else {
+			cout << "RIGHT" << endl;
+		}
+
+	}
+
+}
